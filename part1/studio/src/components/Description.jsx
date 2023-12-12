7c3e3c392e76e02e2304de97ac5bd38344a436d7
Your Name <108307350+pjduri@users.noreply.github.com>
2023-12-12 13:23:15 -0600

    added description component with css module

---
+++ part1/studio/src/components/Description.jsx
@@ -0,0 +1,19 @@
+import styles from './Description.module.css'
+import React from 'react'
+
+export default function() {
+    const authorLink = 'https://panlasangpinoy.com/about-2/'
+    const authorPhoto = 
+        'https://panlasangpinoy.com/wp-content/uploads/2020/03/Vanjo-Merano-Panlasang-Pinoy.jpg'
+    const authorName = 'Vanjo Merano'
+
+    return (
+        <div className={styles.recipeAuthorBlock}>
+            <img src = {authorPhoto} alt='Vanjo Merano' className={styles.imageUpdates} />
+            <div>
+                <h3>{authorName}</h3>
+                <a href={authorLink}>Panlasang Pinoy</a>
+            </div>
+        </div>
+    )
+}
