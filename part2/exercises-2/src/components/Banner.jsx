4e5b25126a9d5f58dabf3c3fb32a981c1c0db0c0
Your Name <108307350+pjduri@users.noreply.github.com>
2023-12-14 10:34:55 -0600

    banner component

--- part2/exercises-2/src/components/Banner.jsx
+++ part2/exercises-2/src/components/Banner.jsx
@@ -1,9 +1,9 @@
-import './styles.css';
+import './styles.css'
 
 function Banner(){
    return(
-      <h1>Hello, World!</h1>
-   );
+      <h1 className='header'>The Amazing Ocean</h1>
+   )
 }
 
-export default Banner;
+export default Banner
