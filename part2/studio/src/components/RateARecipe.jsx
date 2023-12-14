94942c9c41f69917af65d25e927089a0442f897d
Your Name <108307350+pjduri@users.noreply.github.com>
2023-12-14 11:37:55 -0600

    RateARecipe component

--- part2/studio/src/components/RateARecipe.jsx
+++ part2/studio/src/components/RateARecipe.jsx
@@ -1,9 +1,11 @@
-let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
+let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"]
 
-function RateARecipe() {
-  return stars;
+const GiveRating = ({ rating }) => <h3>{rating >=1 && rating <= 5 ? stars[rating - 1] : null}</h3>
+
+function RateARecipe({ rating }) {
+  return <GiveRating rating={rating}/>
 }
 
-export default RateARecipe;
+export default RateARecipe
 
 //conditional to render stars based on a number provided in App.js 
