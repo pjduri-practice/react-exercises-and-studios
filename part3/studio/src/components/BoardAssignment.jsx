0a30cd2fcf37ea5f0c1ef867f81553bfbf02e5f1
Your Name <108307350+pjduri@users.noreply.github.com>
2023-12-18 17:39:58 -0600

    react part 3 studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {
+         label: '-select one-',
+         value: 'no boards yet!'
+      },
+      {
+         label: 'Appetizers',
+         value: 'appetizers'
+      },
+      {
+         label: 'Entrees',
+         value: 'entrees'
+      },
+      {
+         label: 'Desserts',
+         value: 'desserts'
+      }
+   ]
+   const [boardName, setBoardName] = useState('no boards yet!')
 
    const handleChange = (event) => {
+      setBoardName(event.target.value)
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map(b => <option value={b.value}>{b.label}</option>)}
       </select>
 
       <p>Saved to {boardName}!</p>
       </div>
-   );
+   )
 }
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,56 +1,61 @@
-import { useState } from 'react';
+import '../App.css'
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = 'https://panlasangpinoy.com/about-2/'
+   let authorPhoto = 'https://panlasangpinoy.com/wp-content/uploads/2020/03/Vanjo-Merano-Panlasang-Pinoy.jpg'
+   let authorName = 'Vanjo Merano'
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Vanjo Merano" style={{objectFit: "contain", borderRadius: "50%"}} className='imageUpdates' />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink} target='_blank'>Panlasang Pinoy</a> 
          </div>
       </div>
-   );
+   )
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = [
+      '2 lbs pork belly',
+        '2 tablespoons garlic',
+        '5 dry bay leaves',
+        '4 tablespoons vinegar',
+        '1/2 cup soy sauce',
+        '1 tablespoon peppercorn',
+        '2 cups water',
+        'salt to taste'
+   ]
    return(
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredients.map(i => <li>{i}</li>)}
          </ul>
       </div>
-   );
+   )
 }
 
 const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Pork Adobo</h1>
+            <p>My own personal comfort food, filled with memories of my childhood.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
             <RecipeAuthor />
          </div>
       </div>
-   );
+   )
 }
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
-   );
+      <img src='https://panlasangpinoy.com/wp-content/uploads/2009/08/Pork-Adobo-.jpg' alt='Yummy Pork Adobo' className='imageUpdates'/>
+   )
 }
 
 export default function RecipeDisplay () {
@@ -61,5 +66,5 @@ export default function RecipeDisplay () {
             <RecipeDescription />
          </div>
       </div>
-   );
+   )
 }
